cd36d4a738e9de013236bef5127ce69814b934d2
Martin Adamik <martin.adamik@everlution.sk>
2019-05-28 15:19:30 +0200

    #4 base64 src

--- assets/views/notice.php
+++ assets/views/notice.php
@@ -25,4 +25,7 @@
 	<h3 class="filejet-key-status"><?php esc_html_e("FileJet setup and running.", 'filejet'); ?></h3>
 	<p class="filejet-description"><?php printf( __('Check your savings on the stats page.', 'filejet')); ?></p>
 </div>
+<div class="filejet-alert filejet-success">
+    <h3><span class="dashicons dashicons-yes"></span> Your storage ID : <?= Filejet::get_storage_id() ?></h3>
+</div>
 <?php  endif;?>
