f00d1190b9e48331570f5a00468ea00ce29c1ad0
davidkolisek <kolisek.david@gmail.com>
2019-06-05 15:21:00 +0200

    stats widget design

--- assets/views/widget.php
+++ assets/views/widget.php
@@ -3,10 +3,43 @@ $stats = Filejet_Admin::get_statistics_data();
 $breakdown = $stats['breakdown'];
 $currentPeriod = \DateTime::createFromFormat('Y-n', "{$stats['year']}-{$stats['month']}");
 ?>
+<img src="<?php echo esc_url(plugins_url('../images/logo-white.svg', __FILE__)); ?>" alt="" class="logo-white">
 <h3>Stats <?= $currentPeriod->format('F Y') ?></h3>
 <ul>
-    <li><img src="<?php echo esc_url( plugins_url( '../images/master-images.svg', __FILE__ ) ); ?>" alt=""> Master accessed <?= $breakdown['masterImageAccessed'] ?></li>
-    <li><img src="<?php echo esc_url( plugins_url( '../images/renders.svg', __FILE__ ) ); ?>" alt=""> Mutations <?= $breakdown['mutationAccessed'] ?></li>
-    <li><img src="<?php echo esc_url( plugins_url( '../images/total-requests.svg', __FILE__ ) ); ?>" alt=""> Bandwidth <?= Filejet_Admin::format_bytes($breakdown['bandwidth']) ?></li>
-    <li><img src="<?php echo esc_url( plugins_url( '../images/avg-response.svg', __FILE__ ) ); ?>" alt=""> Avg. response (ms) <?= round($breakdown['averageResponseTime']) ?> ms</li>
+    <li>
+        <div class="stats-item">
+            <img src="<?php echo esc_url(plugins_url('../images/master-images.svg', __FILE__)); ?>" alt="">
+            <span>
+               <strong><?= $breakdown['masterImageAccessed'] ?></strong>
+               <strong>Master accessed</strong>
+              </span>
+        </div>
+    </li>
+    <li>
+        <div class="stats-item">
+        <img src="<?php echo esc_url(plugins_url('../images/renders.svg', __FILE__)); ?>" alt="">
+        <span>
+            <strong><?= $breakdown['mutationAccessed'] ?></strong>
+        <strong>Mutations</strong>
+            </span>
+        </div>
+    </li>
+    <li>
+        <div class="stats-item">
+        <img src="<?php echo esc_url(plugins_url('../images/total-requests.svg', __FILE__)); ?>" alt="">
+        <span>
+            <strong><?= Filejet_Admin::format_bytes($breakdown['bandwidth']) ?></strong>
+                <strong>Bandwidth</strong>
+            </span>
+        </div>
+    </li>
+    <li>
+        <div class="stats-item">
+        <img src="<?php echo esc_url(plugins_url('../images/avg-response.svg', __FILE__)); ?>" alt="">
+        <span>
+        <strong><?= round($breakdown['averageResponseTime']) ?> ms</strong>
+        <strong>Avg. response (ms)</strong>
+        </span>
+        </div>
+    </li>
 </ul>
